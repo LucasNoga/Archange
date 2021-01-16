9900146f29356aab38cf2acbf2199c40bab8d9c1
Lucas <luc4snoga@gmail.com>
2021-01-16 18:43:17 +0100

    Update password and config

--- save_nas_history.sh
+++ save_nas_history.sh
@@ -13,7 +13,11 @@ VERSION=1.0.0
 typeset -A COLORS
 COLORS=([red]='\033[0;31m'
     [green]='\033[0;32m'
+    [yellow]='\033[0;33m'
     [nc]='\033[0m') # No Color
+
+# NAS credentials and endpoint (ip + port) 
+typeset -A NAS
 
 ###
 # Main body of script starts here
@@ -22,22 +26,34 @@ main() {
     read_config
 
     folder=$(get_folder $1)
-    echo folder: $folder
+    echo -e Folder when you save your history: ${COLORS[yellow]}$(pwd)${COLORS[nc]}
 
-    #TODO if password is already set no need to call this function
-    get_nas_admin_password
-    echo ""
+    get_password
 
     create_nas_history
     copy_history_local $folder
 }
 
 ###
-# Get variable from settings.file
+# Check if config are ok and get variables from settings.file
 ###
 read_config() {
-    # TODO detect if config file exit and if variables are set
-    source ./settings.conf
+    FILE="./settings.conf"
+    if [ -f "$FILE" ]; then
+        source ./settings.conf
+
+        # Mapping config
+        if [ ! -z ${NAS_USER+x} ]; then NAS["user"]=$NAS_USER; else echo "pas ok user"; fi
+        if [ ! -z ${NAS_PASSWORD+x} ]; then NAS["password"]=$NAS_PASSWORD; else echo "pas ok pass"; fi
+        if [ ! -z ${NAS_IP+x} ]; then NAS["ip"]=$NAS_IP; else echo "pas ok ip"; fi
+        if [ ! -z ${NAS_PORT+x} ]; then NAS["port"]=$NAS_PORT; else echo "pas ok port"; fi
+
+        #echo "ALL CONFIG VALUES:" ${NAS[*]}
+    else
+        echo -e "${COLORS[red]}ERROR: $FILE doesn't exists.${COLORS[nc]}\nExiting..."
+        exit 1
+    fi
+
 }
 
 ###
@@ -52,18 +68,22 @@ get_folder() {
 }
 
 ###
-# Read admin password asked
+# Read admin password asked if it's not set in config
 ###
-get_nas_admin_password() {
-    read -s -p "Type your nas admin password: " NAS_PASSWORD
+get_password() {
+    if [ -z ${NAS[password]} ]; then
+        read -s -p "Type your nas admin password: " NAS[password]
+        echo 
+    fi
 }
 
 ###
 # Create ssh connection to nas and create the NASHISTORY.txt file
 ###
 create_nas_history() {
-    echo "Connexion to the NAS..."
-    sshpass -p $NAS_PASSWORD ssh $NAS_USER@$NAS_IP -p $NAS_PORT "cd /volume1 && ls -R NAS/ > NASHISTORY.txt"
+    echo "Creating NAS history..."
+    echo "Connection to the NAS..."
+    sshpass -p ${NAS[password]} ssh ${NAS[user]}@${NAS[ip]} -p ${NAS[port]} "cd /volume1 && ls -R NAS/ > NASHISTORY.txt"
     ret=$?
     if [ $ret -eq 0 ]; then
         echo -e "${COLORS[green]}History created${COLORS[nc]}"
@@ -77,8 +97,11 @@ create_nas_history() {
 # Copy history file from nas to local
 ###
 copy_history_local() {
+    echo "Copy History in local machine..."
     folder=$1
-    sshpass -p $NAS_PASSWORD scp -P $NAS_PORT $NAS_USER@$NAS_IP:/volume1/NASHISTORY.txt $folder/NASHISTORY_$(date +"%Y-%m-%d").txt
+
+    echo "Connection to the NAS..."
+    sshpass -p ${NAS[password]} scp -P ${NAS[port]} ${NAS[user]}@${NAS[ip]}:/volume1/NASHISTORY.txt $folder/NASHISTORY_$(date +"%Y-%m-%d").txt
     ret=$?
     if [ $ret -eq 0 ]; then
         echo -e "${COLORS[green]}History retrieve${COLORS[nc]}"
